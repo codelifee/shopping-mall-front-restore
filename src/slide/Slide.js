12037bdbee7a657be122a75a330b72105bbb3f40
Choe-song-i <73867587+Choe-song-i@users.noreply.github.com>
2021-02-07 18:15:13 +0900

    Update Slide.js

--- src/slide/Slide.js
+++ src/slide/Slide.js
@@ -4,11 +4,11 @@ import 'slick-carousel/slick/slick.css'// slider library
 import 'slick-carousel/slick/slick-theme.css'// slider library
 import React from 'react';
 import {Link} from 'react-router-dom';
-import main_1 from '../img/herb_2_1.jpg'
-import main_2 from '../img/herb_2_2.jpg'
-import main_3 from '../img/herb_2_3.jpg'
-
-
+import main_1 from '../img/onion.jpg'
+import main_2 from '../img/apple.jpg'
+import main_3 from '../img/pumpkin.jpg'
+import main_4 from '../img/pomegranate.jpg'
+import main_5 from '../img/cabbage.jpg'
 function Slide() {
   let settings = {
     dots: true,
@@ -74,7 +74,7 @@ Herb refers to a plant grown in the West for use as a flavor or medicine.</span>
         </div><div className="card-wrapper">
             <div className="card">
                 <div className="card-image">
-                    <img src={main_1} />
+                    <img src={main_4} />
                     
                 </div>
                 <ul className="colors">
@@ -92,7 +92,7 @@ Herb refers to a plant grown in the West for use as a flavor or medicine.</span>
         <div className="card-wrapper">
             <div className="card">
                 <div className="card-image">
-                    <img src={main_2} />
+                    <img src={main_5} />
                 </div>
                 <ul className="colors">
                     <li><a href="#" className="color color5">A</a></li>
@@ -106,23 +106,7 @@ Herb refers to a plant grown in the West for use as a flavor or medicine.</span>
                 </div>
             </div>
         </div>
-        <div className="card-wrapper">
-            <div className="card">
-                <div className="card-image">
-                    <img src={main_3} />
-                </div>
-                <ul className="colors">
-                    <li><a href="#" className="color color9">A</a></li>
-                    <li><a href="#" className="color color10">B</a></li>
-                    <li><a href="#" className="color color11">C</a></li>
-                    <li><a href="#" className="color color12">D</a></li>
-                </ul>
-                <div className="details">
-                    <h2>Herb<span className="job-title">
-Herb refers to a plant grown in the West for use as a flavor or medicine.</span></h2>
-                </div>
-            </div>
-        </div>
+       
         
         
       
