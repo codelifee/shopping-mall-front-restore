008a1efdaff8698888a789acc9fb2cfe5ac60abb
yunkyeongjang <strangebride@naver.com>
2021-03-01 17:27:16 +0900

    clear git cache

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import "./App.css";
-import Header from "./Header";
-import React, { useState, useEffect } from "react";
-import Home from "./home/Home";
-import Seller from "./seller/Seller";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Checkout from "./checkout/Checkout";
-import Detail from "./detail/Detail";
-import Login from "./authentication/Login";
-import { auth } from "./configuration/firebase";
-import { useStateValue } from "./StateProvider/StateProvider";
-import Payment from "./payment/Payment";
-import Footer from "./footer/Footer";
+import './App.css';
+import Header from './Header';
+import React, { useState, useEffect } from 'react';
+import Home from './home/Home';
+import Seller from './seller/Seller';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './checkout/Checkout';
+import Detail from './detail/Detail';
+import Login from './authentication/Login';
+import { auth } from './configuration/firebase';
+import { useStateValue } from './StateProvider/StateProvider';
+import Payment from './payment/Payment';
+import Footer from './footer/Footer';
 // import ImgSlide from "./slide/ImgSlide";
-import Slide2 from "./slide2/Slide2";
-import LandingPage from "./landingpage/LandingPage";
-import ProductView from "./ProductView/ProductView";
-import ReviewForm from "./detail/ReviewForm";
-import ReviewPatchForm from "./detail/ReviewPatchForm";
-import "./App.css";
-import QnAForm from "./detail/QnAForm";
-import KakaoMap from "./Introduction/KakaoMap";
+import Slide2 from './slide2/Slide2';
+import LandingPage from './landingpage/LandingPage';
+import ProductView from './ProductView/ProductView';
+import ReviewForm from './detail/ReviewForm';
+import ReviewPatchForm from './detail/ReviewPatchForm';
+import './App.css';
+import QnAForm from './detail/QnAForm';
+import KakaoMap from './Introduction/KakaoMap';
 import User from './user/User';
 import LoginForm from './authentication/LoginForm';
 
 function App() {
-
   // const [{}, dispatch] = useStateValue();
 
   // useEffect(() => {
@@ -53,7 +52,7 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
-        {/* <Route path="/map">
+          {/* <Route path="/map">
             <Map />
           </Route> */}
           <Route path="/home">
@@ -66,7 +65,7 @@ function App() {
             <Login />
           </Route>
           <Route path="/loginform">
-          <Header />
+            <Header />
             <LoginForm />
             <Footer />
           </Route>
@@ -81,11 +80,13 @@ function App() {
           <Route path="/products/:id">
             <Header />
             <ProductView />
+            <Footer />
           </Route>
 
           <Route path="/detail/:id">
             <Header />
             <Detail />
+            <Footer />
           </Route>
 
           <Route path="/review/:id">
@@ -114,12 +115,12 @@ function App() {
           </Route>
 
           <Route path="/user">
-              <User />
-            </Route>
+            <User />
+          </Route>
 
           <Route path="/introduction">
             <Header />
-            
+
             <KakaoMap />
             <Footer />
           </Route>
@@ -127,7 +128,6 @@ function App() {
           <Route path="/">
             <LandingPage />
           </Route>
-
         </Switch>
       </div>
     </Router>
