59aeac4ea0d6feae0c2a9ba4053a03da6cb9d5dd
codelifee <suminjung09@gmail.com>
2021-03-03 14:25:28 +0900

    feat: add backend url https(which has ssh certificate) added-by:sumin

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL:
-     "http://shoppingmall-env.eba-jac9afx7.us-east-1.elasticbeanstalk.com/"
+     "https://shoppingmall-env.eba-jac9afx7.us-east-1.elasticbeanstalk.com/"
     //'http://localhost:5000/',
 });
 
