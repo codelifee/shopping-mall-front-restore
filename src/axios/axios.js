8f4434c5d044fe80b648fe4d2166eaefb665b572
codelifee <suminjung09@gmail.com>
2021-03-05 10:54:09 +0900

    fix: fix backend api added ssh to load balancer and subdomain to the domain fixed-by: sumin

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL:
-     "https://shoppingmall-env.eba-jac9afx7.us-east-1.elasticbeanstalk.com/"
+     "https://api.xn--vx3b30no7b.com/"
     //'http://localhost:5000/',
 });
 
