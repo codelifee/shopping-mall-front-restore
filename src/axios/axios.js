852c83a9a93410a8e99a37993db76d98ba71c53b
hey880 <hey880@naver.com>
2021-03-05 14:49:36 +0900

    feat: update axios baseURL and images URL added-by: yeji

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,13 +2,9 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL:
-<<<<<<< HEAD
      "https://api.xn--vx3b30no7b.com/"
-    //'http://localhost:5000/',
-=======
-     //"https://shoppingmall-env.eba-jac9afx7.us-east-1.elasticbeanstalk.com/"
-    'http://localhost:5000/'
->>>>>>> 2912e93eb268c113c138ec55865effe1d02e7485
+    // 'http://localhost:5000/'
+
 });
 
 export default instance;
