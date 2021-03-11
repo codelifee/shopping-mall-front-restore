63c0686b10e08226bd6f6632cb0d221f55775130
yunkyeongjang <strangebride@naver.com>
2021-03-11 23:04:59 +0900

    fix Q&A ui added:yun

--- src/authentication/Login.js
+++ src/authentication/Login.js
@@ -1,83 +1,84 @@
-import React, { useState } from 'react'
-import './Login.css'
-import {Link, useHistory} from "react-router-dom";
-import logo from '../img/logo.png'
-import { auth } from '../configuration/firebase'
-import axios from '../axios/axios'
-import {useStateValue} from '../StateProvider/StateProvider'
-
+import React, { useState } from 'react';
+import './Login.css';
+import { Link, useHistory } from 'react-router-dom';
+import logo from '../img/logo.png';
+import { auth } from '../configuration/firebase';
+import axios from '../axios/axios';
+import { useStateValue } from '../StateProvider/StateProvider';
 
 function Login() {
-    const history = useHistory();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [loginUser, setLoginUser] = useState();
+  const history = useHistory();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [loginUser, setLoginUser] = useState();
 
-    const [{user}, dispatch] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
 
-    // const removeFromBasket = () => {
-    //     dispatch({
-    //         type: 'REMOVE_FROM_BASKET',
-    //         id: id,
-    //     })
-    // }
+  // const removeFromBasket = () => {
+  //     dispatch({
+  //         type: 'REMOVE_FROM_BASKET',
+  //         id: id,
+  //     })
+  // }
 
+  const signIn = (e) => {
+    e.preventDefault();
 
-    const signIn = e => {
-        e.preventDefault();
+    axios
+      .get('/users/login', {
+        params: {
+          user_id: email,
+          user_pwd: password,
+        },
+      })
+      .then((res) =>
+        dispatch({
+          type: 'SET_USER',
+          user: res.data,
+        }),
+      )
+      .catch((err) => console.log(err));
+  };
 
-        axios.get('/users/login', {
-            params: {
-                user_id: email,
-                user_pwd: password
-            }
-        })
-        .then(res => dispatch({
-            type: 'SET_USER',
-            user: res.data
-        }))
-        .catch(err => console.log(err));
-    }
+  console.log(loginUser);
 
-    console.log(loginUser)
+  return (
+    <div className="login">
+      <Link to="/home">
+        <img className="login__logo" src={logo} />
+      </Link>
 
-    return (
-        <div className='login'>
-            <Link to='/home'>
-            <img className="login__logo" src={logo} />
-            </Link>
-            
-            <div className='login__container'>
-                <h1>Sign in</h1>
-                <form>
-                    <h5>Id</h5>
-                    <input 
-                    name="email"
-                    type='text' 
-                    onChange={e => setEmail(e.target.value)}
-                    />
+      <div className="login__container">
+        <h1>Sign in</h1>
+        <form>
+          <h5>Id</h5>
+          <input
+            name="email"
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-                    <h5>Password</h5>
-                    <input 
-                    name="password"
-                    type='password'
-                    onChange={e => setPassword(e.target.value)}
+          <h5>Password</h5>
+          <input
+            name="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-                    />
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
+        </form>
 
-                    <button
-                    type='submit'
-                    onClick={signIn}
-                    className='login__signInButton'>Sign In</button>
-                </form>
-
-                {/* <button 
+        {/* <button 
                 onClick={register}
                 className='login__registerButton'>Create Account</button> */}
-
-            </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 export default Login;
-
--- src/seller/AnsweredCategory.js
+++ src/seller/AnsweredCategory.js
@@ -5,8 +5,7 @@ import DatePicker from 'react-datepicker';
 import axios from '../axios/axios';
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import { Category } from '@material-ui/icons';
@@ -79,51 +78,49 @@ function AnsweredCategory() {
             />
           </div>
         </div>
-        
-        <div className="question__info">
-       <p className="answer_page">답변완료 페이지</p>
-         <p className="answer_page1">{totalQuestion.length} Questions</p>
-         
-        </div>
-       <div className="category__table_bg">
-          <table className="category__table">
-            <thead>
-              <th>카테고리명</th>
-              <th>답변완료된 질문개수</th>
-              <th>답변하기</th>
-            </thead>
-            <tbody>
-              {categories.map((category) => {
-                let categoryQuestion = null;
-                categoryQuestion = question
-                  .filter((val) => {
-                    return (
-                      val.category_id == category.category_id &&
-                      val.answer !== null
-                    );
-                  })
-                  .map((val) => {
-                    return val.question_id;
-                  });
-                return (
-                  <tr key={category.category_id}>
-                    
-                      <td>
-                        <span>{category.category_name} </span>
-                      </td>
-                    
 
-                    <td>{categoryQuestion.length}</td>
-                    <td>
-                      <Link to={`/seller/answeredProducts/${category.category_id}`}
-                  ><i class="fas fa-pencil-alt"></i></Link></td>
-                  
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+        <div className="question__info">
+          <p className="answer_page">답변완료 페이지</p>
+          <p className="answer_page1">{totalQuestion.length} Questions</p>
         </div>
+        <table className="category__table">
+          <thead>
+            <th>카테고리명</th>
+            <th>답변완료된 질문개수</th>
+            <th>답변하기</th>
+          </thead>
+          <tbody>
+            {categories.map((category) => {
+              let categoryQuestion = null;
+              categoryQuestion = question
+                .filter((val) => {
+                  return (
+                    val.category_id == category.category_id &&
+                    val.answer !== null
+                  );
+                })
+                .map((val) => {
+                  return val.question_id;
+                });
+              return (
+                <tr key={category.category_id}>
+                  <td>
+                    <span>{category.category_name} </span>
+                  </td>
+
+                  <td>{categoryQuestion.length}</td>
+                  <td>
+                    <Link
+                      to={`/seller/answeredProducts/${category.category_id}`}
+                    >
+                      <i class="fas fa-pencil-alt"></i>
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
